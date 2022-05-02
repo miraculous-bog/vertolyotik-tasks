73a7d820b4fc7d6632981633d962dce0884c3817
Bogdan <bogdan9110@gmail.com>
2022-05-02 18:55:20 +0300

    ready task

--- patterns/script.js
+++ patterns/script.js
@@ -1,4 +1,6 @@
 'use strict';
+// працюємо з класом ImageEditor. Для того аби створити екземпляр вікна графічних елемнтів створюємо змінну від цього класу.
+// для побудови дерева з графічних елементів використано паттерн компонувальник
 
 const isArray = Array.isArray || function (arr) {
     return Object.prototype.toString.call(arr) === "[object Array]";
@@ -8,18 +10,80 @@ const isObject = function (obj) {
     return Object.prototype.toString.call(obj) === "[object Object]";
 }
 
+const getTree = (ob) => {
+    const otherObj = [];
+    function three(tr) {
+        var paths = [];
 
-class Graphic {
-    constructor() { }
+        function findPath(branch, str) {
+            Object.keys(branch).forEach(function (key) {
+                if (isArray(branch[key]) || isObject(branch[key])) {
+                    findPath(branch[key], str ? str + "." + key : key);
+                    // console.log(branch, key, branch[key]);
+                }
+                else {
+                    paths.push(str ? str + ' ' + branch.type + '_' + branch.id : key + ' ' + branch.type + ' ' + branch.id);
+                    // console.log(branch);
 
-    move(x, y) {
-        console.log('moving on', x, y)
+                }
+            });
+        }
+
+        findPath(tr, "");
+        return paths;
     }
 
-    draw() {
-        console.log('drawing')
+
+    const getClearTree = (str) => {
+        const newArr = str.map(item => {
+            let index = 0;
+            for (let i = item.length - 1; i < item.length; i--) {
+                if (item[i] === '.') {
+                    index = i;
+                    break;
+                }
+
+            }
+            return item.slice(3);
+        });
+        const prepearedArr = [];
+        newArr.forEach(item => {
+            if (item !== prepearedArr[prepearedArr.length - 1]) {
+                prepearedArr.push(item);
+            }
+        })
+        return prepearedArr;
+    };
+
+    const treeElemnts = three(ob);
+    const editedArrStr = getClearTree(treeElemnts);
+    const getGraficTree = (arr) => {
+
+
+        const findInterface = (str, index) => {
+            // console.log(str, str[11])
+            if (str[11] === ' ') {
+                console.log(`${index}${str.split(" ")[1]}`);
+                otherObj.push({ count: index, id: str.split(" ")[1] });
+            } else {
+                console.log(`${index}group`);
+                index += '+';
+                findInterface(str.slice(11), index);
+            }
+        }
+
+
+        for (let i = 0; i < arr.length; i++) {
+            let counter = '';
+            findInterface(arr[i], counter);
+        }
+
+        console.log("console.log(newIE.getObj(''));");
     }
+    getGraficTree(editedArrStr);
+    return otherObj;
 }
+
 
 class Dot {
     getId() {
@@ -32,10 +96,16 @@ class Dot {
         this.y = y;
     }
 
-    move(x, y) {
-        this.x += x;
-        this.y += y;
-        console.log('moving on', x, y)
+    move(x, y, destination) {
+        if (destination) {
+            this.x += x;
+            this.y += y;
+            console.log('moving on', x, y)
+        } else {
+            this.x -= x;
+            this.y -= y;
+            console.log('moving on -', x, y)
+        }
     }
 
     draw() {
@@ -46,18 +116,24 @@ class Dot {
 
 class Circle extends Dot {
 
-    constructor(x, y, id, radius) {
+    constructor(x, y, id, parameters) {
         super(x, y, id);
         this.type = 'circle';
-        this.radius = radius;
+        this.parameters = parameters;
     }
 
-    draw() {
-        console.log('drawing circle')
-    }
 
 }
+class Rectangle extends Dot {
 
+    constructor(x, y, id, parameters) {
+        super(x, y, id);
+        this.type = 'rectangle';
+        this.parameters = parameters;
+    }
+
+
+}
 class CompoundGraphic {
     children = [];
 
@@ -66,19 +142,26 @@ class CompoundGraphic {
         this.children.push(graphic);
     }
 
-    remove(graphic) {
-        console.log(`${graphic} was deleted`);
+    remove(id) {
+        const index = this.children.indexOf(this.children.find(el => el.id === id));
+        this.children.splice(index, 1);
+        console.log(`${id} was deleted`);
     }
 
-    move(x, y) {
+
+    moveGraphicGroup(x, y, destination) {
         this.children.forEach(item => {
-            item.x += x;
-            item.y += y;
-            console.log(`${item} was changed them position`);
+            if (destination) {
+                item.x += x;
+                item.y += y;
+                console.log(`${item} was changed them position`);
+            } else {
+                item.x -= x;
+                item.y -= y;
+                console.log(`${item} was changed them position`);
+            }
         });
-
     }
-
 
 }
 
@@ -86,27 +169,50 @@ class CompoundGraphic {
 class ImageEditor {
     all = [];
 
-    get all() {
-        return this.all[2];
+    static repeatData = {
+        data: [],
+        action: "",
     }
-
-    // set all(value) {
-    //     this.name = value;
-    // }
-
+    static backupData = {
+        data: [],
+        action: "",
+    }
     load() {
         this.all = new CompoundGraphic();
         console.log('all', this.all);
-        this.all.add(new Dot(1, 2));
-        this.all.add(new Circle(5, 3, 10));
-        // console.log('ImageEditor-> load')
+        this.all.add(new Circle(50, 20, '', [50, 50]));
+        this.all.add(new Rectangle(100, 300, '', [20, 30]));
     }
-    groupSelected(arrGraphics) {
+    addElement(element, otherGroup = null) {
+        if (otherGroup === null) {
+            this.all.add(element)
+        } else {
+            const item = this.getGroup(otherGroup);
+            item.add(element);
+        }
+        ImageEditor.repeatData.data = [element, otherGroup];
+        ImageEditor.repeatData.action = this.addElement;
+
+
+        ImageEditor.backupData.data = [element.id];
+        ImageEditor.backupData.action = this.deleteGraphic;
+        this.drawing();
+    }
+    groupSelected(arrGraphics, otherGroup = null) {
         const group = new CompoundGraphic();
         arrGraphics.forEach(component => {
             group.add(component);
         });
-        this.all.add(group)
+        if (otherGroup === null) {
+            this.all.add(group)
+        } else {
+            const item = this.getGroup(otherGroup);
+            item.add(group);
+        }
+        ImageEditor.repeatData.data = [arrGraphics, otherGroup];
+        ImageEditor.repeatData.action = this.groupSelected;
+        this.drawing();
+
     }
 
     getObj(id, type) {
@@ -116,12 +222,11 @@ class ImageEditor {
             Object.keys(branch).forEach(function (key) {
                 if (isArray(branch[key]) || isObject(branch[key])) {
                     findPath(branch[key], str ? str + "." + key : key, branch);
-                    // console.log(branch, key, branch[key]);
                 }
                 else {
-                    // paths.push(str ? str + ' ' + branch.type + '_' + branch.id : key + ' ' + branch.type + ' ' + branch.id);
-                    // console.log(branch);
+
                     if (branch.id === id && type === "element") { paths = branch; } else if (branch.id === id && type === "group") {
+                        console.log("----------");
                         paths = prevBranch;
                     }
                     return;
@@ -132,106 +237,247 @@ class ImageEditor {
         findPath(this.all, "");
         return paths;
     }
+    getGroup(id) {
+        var paths = [];
 
-}
-// console.log(CompoundGraphic);
-// console.log(ImageEditor);
-// ImageEditor.load;
+        function findPath(branch, prevBranch = null) {
+            Object.keys(branch).forEach(function (key) {
+                if (branch[key].children || isArray(branch[key])) {
+                    findPath(branch[key], branch);
+                }
+                else {
+                    if (branch[key].id === id) {
+                        paths = prevBranch;
+                    }
+                }
+            });
+        }
+
+        findPath(this.all, "");
+        return paths;
+    }
+
+    getElm(el) {
+        if (el.type === "dot") {
+            return new Dot(el.x, el.y);
+        } else if (el.type === "circle") {
+            return new Circle(el.x, el.y, el.radius);
+        }
+    }
+
+    moveGraphic(id, x, y, destination) {
+        const graphic = this.getObj(id, 'element');
+        console.log(graphic);
+        graphic.move(x, y, destination);
+        ImageEditor.repeatData.data = [id, x, y, destination];
+        ImageEditor.repeatData.action = this.moveGraphic;
+
+        ImageEditor.backupData.data = [id, -x, -y, destination ? false : true];
+        ImageEditor.backupData.action = this.moveGraphic;
+        this.drawing();
+
+    }
+    moveGraphicGroup(id, x, y, destination) {
+        const group = this.getGroup(id);
+        console.log(group);
+        group.moveGraphicGroup(x, y, destination);
+
+        ImageEditor.repeatData.data = [id, x, y, destination];
+        ImageEditor.repeatData.action = this.moveGraphicGroup;
 
 
-// ImageEditor.load;
-const newIE = new ImageEditor();
+        ImageEditor.backupData.data = [id, -x, -y, destination ? false : true];
+        ImageEditor.backupData.action = this.moveGraphicGroup;
+        this.drawing();
 
-newIE.load();
-newIE.groupSelected([new Circle(5, 5, 10), new Circle(2, 2, 4)]);
-newIE.groupSelected([]);
-// newIE.groupSelected([new Circle(5, 5, 10), new Circle(2, 2, 4)]);
-
-console.log(newIE);
-// console.log(newIE.all.load(new Circle(1, 1, 1)));
-// console.dir(newIE.all);
-newIE.groupSelected(newIE.all.children);
-// newIE.load()
-console.dir(newIE);
+    }
 
 
+    deleteGraphic(id) {
+        const graphic = this.getGroup(id);
+        const backupInfo = this.getObj(id, "element");
+        ImageEditor.backupData.data = [this.getElm(backupInfo), graphic];
+        graphic.remove(id);
+        console.log(graphic);
 
-function three(tr) {
-    var paths = [];
+        ImageEditor.backupData.action = this.addElement;
+        this.drawing();
 
-    function findPath(branch, str) {
-        Object.keys(branch).forEach(function (key) {
-            if (isArray(branch[key]) || isObject(branch[key])) {
-                findPath(branch[key], str ? str + "." + key : key);
-                // console.log(branch, key, branch[key]);
+    }
+
+    shiftGraphic(id, group = null) {
+
+
+        const currentGraphic = this.getObj(id, "element");
+        const graphic = this.getElm(currentGraphic);
+        const gr = this.getGroup(id);
+        gr.remove(id);
+        this.addElement(graphic, group);
+        console.log(graphic);
+
+
+        ImageEditor.repeatData.data = [id, group];
+        ImageEditor.repeatData.action = this.shiftGraphic;
+        this.drawing();
+
+    }
+
+    repeat() {
+        const calling = ImageEditor.repeatData.action.bind(this, ...ImageEditor.repeatData.data);
+        calling();
+    }
+
+    backupData() {
+        const calling = ImageEditor.backupData.action.bind(this, ...ImageEditor.backupData.data);
+        calling();
+    }
+
+    drawing(thisObj) {
+        const paths = [];
+        let counter = 0;
+        function findPath(branch, str) {
+            Object.keys(branch).forEach(function (key) {
+                if (isArray(branch[key]) || isObject(branch[key])) {
+                    findPath(branch[key]);
+                    counter++;
+                }
+                else {
+                    paths.push({ ...branch });
+                }
+            });
+        }
+
+        findPath(this.all, "");
+        const answr = [];
+        answr.push({ ...paths[0] });
+
+        for (let i = 1; i < paths.length; i++) {
+            if (paths[i].id !== paths[i - 1].id) {
+                answr.push({ ...paths[i] });
             }
-            else {
-                paths.push(str ? str + ' ' + branch.type + '_' + branch.id : key + ' ' + branch.type + ' ' + branch.id);
-                // console.log(branch);
+        }
+        // return answr;
+        const gettingTree = getTree(this);
+        const editedRightIdTree = gettingTree.map(item => {
+            const newId = item.id.split("_")[1];
+            return { ...item, id: newId }
+        })
+        const prepearedDataItem = editedRightIdTree.map(item => {
+            let match = answr.find(el => el.id === item.id);
 
+
+            return { ...match, zIndex: item.count.length };
+        });
+        const colorCircle = ["#660099", "#9900FF", "#9933FF", "#9966FF", "#CC99FF"];
+        const colorRectangle = ["#006600", "#339933", "#66CC66", "#99FF99", "#CCFFCC"];
+        let strElements = "";
+
+        const getColor = (num, type) => {
+            if (type === "circle") {
+                if (num > 4) {
+                    return colorCircle[5];
+                } else {
+                    return colorCircle[num];
+                }
+            } else if (type === "rectangle") {
+                if (num > 4) {
+                    return colorRectangle[5];
+                } else {
+                    return colorRectangle[num];
+                }
+            }
+        }
+        prepearedDataItem.forEach(item => {
+            let str;
+            if (item.type === "circle") {
+                str = `<div class=${item.type}
+                style="height: ${item.parameters[0]}px;width: ${item.parameters[1]}px;border-radius: 50%;background-color: ${getColor(item.zIndex, item.type)};z-index: ${100 - item.zIndex};top: ${item.y}px;left: ${item.x}px;"></div>`;
+                strElements += str;
+            } else if (item.type === "rectangle") {
+                str = `<div class="${item.type}" style="height: ${item.parameters[0]}px;width: ${item.parameters[1]}px;background-color: ${getColor(item.zIndex, item.type)};z-index: ${100 - item.zIndex};top: ${item.y}px;left: ${item.x}px;"></div>`;
+                strElements += str;
             }
         });
-    }
 
-    findPath(tr, "");
-    return paths;
-}
-
-const treeElemnts = three(newIE);
+        const wrapper = document.querySelector(".wrapper");
 
 
-const getClearTree = (str) => {
-    const newArr = str.map(item => {
-        let index = 0;
-        for (let i = item.length - 1; i < item.length; i--) {
-            if (item[i] === '.') {
-                index = i;
-                break;
+        const verifyingPosition = (positionData) => {
+            const subArr = prepearedDataItem.filter(item => item.id);
+            return subArr.filter(item => {
+                let x = positionData.left;
+                let y = positionData.top;
+                let x1 = item.x;
+                let y1 = item.y;
+                let x2 = x1 + item.parameters[0];
+                let y2 = y1 + item.parameters[1];
+                if ((x <= x2 && x >= x1) && (y <= y2 && y >= y1)) {
+                    return item;
+                }
+            });
+        }
+
+        const handlerClick = (event) => {
+            // var rect = event;
+            const currentPointElm = verifyingPosition({
+                left: event.pageX,
+                top: event.pageY,
+            });
+
+            if (currentPointElm.length !== 0) {
+                const indexArr = currentPointElm.map(item => item.zIndex);
+                const maxNum = Math.max(...indexArr);
+                const searchedEl = currentPointElm.find(item => item.zIndex === maxNum);
+                alert(`найглибший» елемент в цій точцi\ntype: ${searchedEl.type}\nx: ${searchedEl.x}\ny: ${searchedEl.y}\nwidth and height: ${searchedEl.parameters[0]} ${searchedEl.parameters[1]}`);
             }
 
         }
-        // console.log(index);
-        return item.slice(3);
-    });
-    const prepearedArr = [];
-    // console.log(newArr);
-    newArr.forEach(item => {
-        if (item !== prepearedArr[prepearedArr.length - 1]) {
-            prepearedArr.push(item);
-        }
-    })
-    return prepearedArr;
-};
-// console.log(treeElemnts);
-// getClearTree(treeElemnts);
-const editedArrStr = getClearTree(treeElemnts);
-// console.log(editedArrStr);
+        wrapper.innerHTML = '';
+        let mainStr = `<div class="window">${strElements}</div>`
+        wrapper.insertAdjacentHTML('afterbegin', mainStr)
 
-const getGraficTree = (arr) => {
-
-
-    const findInterface = (str, index) => {
-        // console.log(str, str[11])
-        if (str[11] === ' ') {
-            console.log(`${index}${str.split(" ")[1]}`);
-        } else {
-            console.log(`${index}group`);
-            index += '+';
-            findInterface(str.slice(11), index);
-        }
+        document.querySelector("body").addEventListener("click", handlerClick);
     }
 
-
-    for (let i = 0; i < arr.length; i++) {
-        let counter = '';
-        findInterface(arr[i], counter);
-    }
-
-    console.log("console.log(newIE.getObj(''));");
 }
-getGraficTree(editedArrStr);
 
-// getGraficTree(getClearTree(treeElemnts), 1);
+// створюємо екземпляр головного класу контролю елементів та груп
+const newIE = new ImageEditor();
+
+// завантажуємо стартові елементи за доп. методу load()
+newIE.load();
+// в корені нашого дерева створюємо групу в яку завантажуємо коло та прямокутник
+newIE.groupSelected([new Circle(300, 410, '', [10, 10]), new Rectangle(300, 500, '', [100, 50]), new Rectangle(50, 20, "", [100, 100])]);
+// після кожного виклику метода виконується візуальна побудова структури в консолі, також можна викликати за доп. фукнції getTree(obj);
+// додамо новий елемент за допю методу, в параметрах ми передаємо елемент, і другим id групи в яку хочему помістити, якщо id не вказаний то автоматично закинеться в корінь дерева.
+newIE.addElement(new Circle(400, 400, "", [40, 40]));
 
 
-// console.log(newIE.updateCommentRating(''));
+// Також на доступні наступні методи
+
+// рухає фігуру
+// obj.moveGraphic(id,x,y,destination)
+
+// рухає групу фігур
+// moveGraphicGroup(id, x, y, destination)
+
+// видаляє фігуру
+//obj.deleteGraphic(id)
+
+// переміщення в структурі граф. дерева
+// obj.shiftGraphic(id, group = null)
+
+//повторює попередній метод
+// obj.repeat()
+
+// відмінює попередню дію методу
+// obj.backupData()
+
+
+getTree(newIE);
+
+
+
+
+
+
